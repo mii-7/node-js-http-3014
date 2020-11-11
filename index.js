e39ea490747fa5bf44176fc09e057691deb53bf9
mii <mii.prog.7@gmail.com>
2020-11-11 21:10:25 +0900

    HTMLフォームを利用したWebサービス

--- index.js
+++ index.js
@@ -4,26 +4,29 @@ const server = http.createServer((req, res) => {
   const now = new Date();
   console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
 
   switch (req.method) {
     case 'GET':
-      res.write('GET ' + req.url);
+	  const fs = require('fs');
+	  const rs = fs.createReadStream('./form.html');
+	  rs.pipe(res);
       break;
     case 'POST':
-      res.write('POST ' + req.url);
       let rawData = '';
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
       }).on('end', () => {
-        console.info('[' + now + '] Data posted: ' + rawData);
+		const decoded = decodeURIComponent(rawData);
+		console.info('[' + now + '] 投稿: ' + decoded);
+		res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + decoded + 'が投稿されました</h1></body></html>');
+		res.end();
       });
       break;
     default:
       break;
   }
-  res.end();
 }).on('error', (e) => {
   console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
